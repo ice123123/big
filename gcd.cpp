467d5e0219f5d9b80a079ca7dae499a03376dccf
Joshua Jolley <jolleyboy@gmail.com>
2014-06-09 22:52:29 -0600

    Fixed a spelling error

--- gcd.cpp
+++ gcd.cpp
@@ -67,7 +67,7 @@ int main(int argc, char * argv[])
 		if (mygcd == atoi(argv[1]) || mygcd == atoi(argv[2]))
 			cout << argv[1] << " and " << argv[2] << " are coprime.\n";
 		else if (mygcd == 1)
-			cout << "Either " << argv[1] << " or " << argv[2] << " are prime.\n";
+			cout << "Either " << argv[1] << " or " << argv[2] << " is prime.\n";
 		else
 		{
 			cout << "s * a + t * b = gcd\n";

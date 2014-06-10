025293fd8b6e5a36028c20ae1b20b5bd8fcb6c3f
Joshua Jolley <jolleyboy@gmail.com>
2014-06-09 22:47:40 -0600

    added prime case to gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -66,6 +66,8 @@ int main(int argc, char * argv[])
 		int mygcd = gcd(atoi(argv[1]),atoi(argv[2]),s,t);
 		if (mygcd == atoi(argv[1]) || mygcd == atoi(argv[2]))
 			cout << argv[1] << " and " << argv[2] << " are coprime.\n";
+		else if (mygcd == 1)
+			cout << "Either " << argv[1] << " or " << argv[2] << " are prime.\n";
 		else
 		{
 			cout << "s * a + t * b = gcd\n";

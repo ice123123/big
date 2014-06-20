f2f37b666f2bc94cc65a98fc0bbde19daca0db74
Joshua Jolley <jolleyboy@gmail.com>
2014-06-20 13:23:51 -0600

    Here we go again

--- rsaZZ.cpp
+++ rsaZZ.cpp
@@ -74,6 +74,6 @@ ZZ findE(ZZ t)
  ******************************************************************************/
 void findPandQ(ZZ m, ZZ &p, ZZ &q)
 {
-   GenPrime(p, NumBytes(m) * 4;
-   GenGermainPrime(q, (long)NumBytes(m) * 4);
+   GenPrime(p, NumBytes(m) * 4);
+   GenGermainPrime(q, NumBytes(m) * 4);
 }
